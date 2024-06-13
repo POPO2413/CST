c3f9c69412547654fc2e996ce3f427b89f6db52f
POPO2413 <167417869+POPO2413@users.noreply.github.com>
2024-06-13 19:50:17 +0800

    ??

--- .../app.py
+++ .../app.py
@@ -24,7 +24,7 @@ def login():
         password = request.form['password']
         role = request.form['role']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM Data WHERE Name = %s AND password = %s AND Role = %s', (username, password, role))
+        cursor.execute('SELECT * FROM Data WHERE Username = %s AND Password = %s AND Role = %s', (username, password, role))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
@@ -39,9 +39,9 @@ def login():
             elif role == 'student':
                 return redirect(url_for('studentindex'))
             elif role == 'teacher':
-                return redirect(url_for('teacher_index'))
+                return redirect(url_for('teacherindex'))
         else:
-            msg = 'Incorrect username / password !'
+            msg = 'Incorrect Username / Password !'
     return render_template('login.html', msg=msg)
 
 @app.route('/adminindex')
@@ -63,19 +63,18 @@ def teacherindex():
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
+    session.pop('Username', None)
     return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
+    if request.method == 'POST' and 'Username' in request.form and 'Password' in request.form and 'Email' in request.form:
+        username = request.form['Username']
+        password = request.form['Password']
+        email = request.form['Email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM csdata WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM data WHERE Username = %s', (username,))
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists!'
@@ -86,7 +85,7 @@ def register():
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO csdata VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute('INSERT INTO data VALUES (NULL, %s, %s, %s)', (username, password, email,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
@@ -101,7 +100,7 @@ def query_data():
 def search_data():
     query = request.args.get('query')
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute("SELECT * FROM Data WHERE Name LIKE %s", ('%' + query + '%',))
+    cursor.execute("SELECT * FROM data WHERE Username LIKE %s", ('%' + query + '%',))
     search_results = cursor.fetchall()
     return render_template('query_data.html', search_results=search_results)
 
@@ -164,3 +163,10 @@ def settings():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/api/users', methods=['GET'])
+def api_users():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT Username, Email FROM users')
+    users = cursor.fetchall()
+    return {"users": users}
